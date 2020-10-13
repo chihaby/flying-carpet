2c5dde60972f0fdd6781e8033f4d7f8771e82123
Rad <chihaby@gmail.com>
2020-10-13 00:23:38 -0500

    adds mongodb schema

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,23 +1,22 @@
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 
 const users = [
   {
-  name: 'Admin User',
-  email: "admin@example.com",
-  password: bcrypt.hashSync('123456', 10),
-  isAdmin: true
-},
-{
-  name: 'John Doe',
-  email: bcrypt.hashSync('123456', 10),
-  password: xxxx,
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
 
-},
-{
-  name: 'Jane Doe',
-  email: bcrypt.hashSync('123456', 10),
-  password: xxxx,
-},
-]
-
-export default users
+export default users;
