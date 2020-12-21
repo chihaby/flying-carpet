6c489e2534bc387bfd73c1303dd120017c3d23d1
Rad <chihaby@gmail.com>
2020-12-21 10:45:39 +0100

    fixed many errors after a long break

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,11 @@
 import asyncHandler from "express-async-handler";
-import Product from "../models/productModel";
+import Product from "../models/productModel.js";
 
 //@desc  Fetch all products
 //@route  GET /api/products
 //@access  Public
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
-
   res.json(products);
 });
 
@@ -14,7 +13,7 @@ const getProducts = asyncHandler(async (req, res) => {
 //@route  GET /api/products/:id
 //@access  Public
 const getProductById = asyncHandler(async (req, res) => {
-  const products = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id);
 
   if (product) {
     res.json(product);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,6 @@ import express from "express";
 const router = express.Router();
 import { authUser } from "../controllers/userController.js";
 
-router.post("/login", authUser).get(getProducts);
+router.post("/login", authUser);
 
 export default router;
