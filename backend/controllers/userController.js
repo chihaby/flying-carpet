72db2fb3e6b953766cacce87fd72d1e668913b1f
Rad <chihaby@gmail.com>
2021-01-20 10:56:02 +0100

    adds userListScreen component

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -167,14 +167,6 @@ const updateUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc    Get all users
-// @route   GET /api/users
-// @access  Private/Admin
-const getUsers = asyncHandler(async (req, res) => {
-  const users = await User.find({});
-  res.json(users);
-});
-
 export {
   authUser,
   registerUser,
@@ -184,5 +176,4 @@ export {
   deleteUser,
   getUserById,
   updateUser,
-  getUsers,
 };
